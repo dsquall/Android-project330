f080cc432b0fc60b5f50ceb7f59034d8166834dd
dsquall <jgordy1@gulls.salisbury.edu>
2016-04-29 13:29:56 -0400

    new changes

---
+++ app/src/main/java/com/example/jon/movieapp/ChatActivity.java
@@ -0,0 +1,12 @@
+package com.example.jon.movieapp;
+
+import android.app.Application;
+
+public class ChatActivity extends Application {
+
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+}
--- app/src/main/java/com/example/jon/movieapp/MainActivity.java
+++ app/src/main/java/com/example/jon/movieapp/MainActivity.java
@@ -1,12 +1,39 @@
 package com.example.jon.movieapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabHost hostTab = getTabHost();
+        TabHost.TabSpec spec;
+        Intent content;
+
+        spec = tabHost.newTabSpec("Showtimes");
+        spec.setIndicator("Showtimes");
+        content= new Intent(this, MainActivity.class);
+        spec.setContent(content);
+        tabHost.addTab(spec);
+
+
+        spec = tabHost.newTabSpec("myMovies");
+        spec.setIndicator("myMovies");
+        content= new Intent(this, MainActivity.class);
+        spec.setContent(content);
+        tabHost.addTab(spec);
+
+
+        spec = tabHost.newTabSpec("Chat");
+        spec.setIndicator("Chat");
+        content= new Intent(this, MainActivity.class);
+        spec.setContent(content);
+        tabHost.addTab(spec);
+
     }
 }
